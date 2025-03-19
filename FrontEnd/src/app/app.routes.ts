f215a86f2c0829cc1fb8d7a5d0926ea6979676d7
ratte2d3d <kinoyama001@gmail.com>
2025-03-19 10:21:12 +0900

    Update app.routes.ts

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -25,3 +25,9 @@ export const routes: Routes = [
   },
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {
+    useHash: true, // 追加
+  })],
+  exports: [RouterModule]
+})
