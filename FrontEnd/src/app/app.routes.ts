a7bda42f12bcf550c4b5450117d4d31106659886
auio55 <eternityheart0423@gmail.com>
2025-03-19 11:24:20 +0900

    Add useHash

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -1,4 +1,4 @@
-import { Routes, provideRouter, withComponentInputBinding } from '@angular/router';
+import { Routes, provideRouter, withComponentInputBinding, withHashLocation } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CatanComponent } from './catan/catan.component';
 import { PlayerComponent } from './catan/player/player.component';
@@ -25,4 +25,4 @@ export const routes: Routes = [
   },
 ];
 
-export const appRoutes = provideRouter(routes, withComponentInputBinding(), { useHash: true });
+export const appRoutes = provideRouter(routes, withComponentInputBinding(), withHashLocation());
