1aabc36365a263b233c0b4e5ac72e00e3430e863
ratte2d3d <kinoyama001@gmail.com>
2025-03-28 02:37:34 +0900

    AWS設定の追加

--- BackEnd/src/settings.py
+++ BackEnd/src/settings.py
@@ -147,19 +147,17 @@ environ.Env.read_env()
 AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
-# AWS_S3_REGION_NAME = env('AWS_S3_REGION_NAME')
+AWS_S3_REGION_NAME = env('AWS_S3_REGION_NAME')
 # S3にアップロードされるファイルにデフォルトのACLを設定しない
-AWS_DEFAULT_ACL = 'public-read'
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',  # キャッシュの制御（オプション）
-}
-AWS_LOCATION = 'static'
-
-AWS_QUERYSTRING_AUTH = False
-
-AWS_HEADERS = {
-    'Access-Control-Allow-Origin': '*',
-}
+# AWS_DEFAULT_ACL = 'public-read'
+# AWS_S3_OBJECT_PARAMETERS = {
+#     'CacheControl': 'max-age=86400',  # キャッシュの制御（オプション）
+# }
+# AWS_LOCATION = 'static'
+# AWS_QUERYSTRING_AUTH = False
+# AWS_HEADERS = {
+#     'Access-Control-Allow-Origin': '*',
+# }
 
 # メディアファイル（画像など）のURL設定
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
@@ -171,10 +169,10 @@ MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 # STATIC_URL = '/static/'
-STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # mediaファイル保存先
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 # # staticファイルの参照先
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3ManifestStaticStorage'
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3ManifestStaticStorage'
