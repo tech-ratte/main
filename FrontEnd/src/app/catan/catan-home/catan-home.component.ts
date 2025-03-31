f6585d254aa1bf4a7ac5aa025f98ac8e40a6fe81
ratte2d3d <kinoyama001@gmail.com>
2025-04-01 06:25:26 +0900

    excel読み込みテスト

--- FrontEnd/src/app/catan/catan-home/catan-home.component.ts
+++ FrontEnd/src/app/catan/catan-home/catan-home.component.ts
@@ -8,19 +8,21 @@ import * as XLSX from 'xlsx';
   styleUrl: './catan-home.component.scss',
 })
 export class CatanHomeComponent {
-  readStart: string = '';
+  progress: string = '';
   sheetNames: string[] = [];
   data: any;
 
   readExcel(event: any) {
-    this.readStart = '読み取り開始';
+    this.progress = 'ファイル選択完了';
     const target: DataTransfer = <DataTransfer>event.target;
     if (target.files.length !== 1) return;
 
+    this.progress = '読み取り開始';
     const reader: FileReader = new FileReader();
     reader.onload = (e: any) => {
       const binaryString: string = e.target.result;
       const workbook: XLSX.WorkBook = XLSX.read(binaryString, { type: 'binary' });
+      this.progress = 'ワークブック読み込み';
 
       this.sheetNames = workbook.SheetNames;
       const sheetName: string = workbook.SheetNames[0];
@@ -31,6 +33,7 @@ export class CatanHomeComponent {
       const cellAddress = 'B2'; // 取得したいセルの位置（例：B2）
       const cellValue = worksheet[cellAddress]?.v; // 値を取得（?.でnull安全）
       this.data = cellValue;
+      this.progress = '読み取り完了';
     };
   }
 }
