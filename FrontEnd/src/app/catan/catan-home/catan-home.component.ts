eabd96ea05ab8ab1031fa270d8192f5c216f2893
ratte2d3d <kinoyama001@gmail.com>
2025-04-01 06:13:05 +0900

    excel読み込みテスト

--- FrontEnd/src/app/catan/catan-home/catan-home.component.ts
+++ FrontEnd/src/app/catan/catan-home/catan-home.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-catan-home',
   imports: [],
   templateUrl: './catan-home.component.html',
-  styleUrl: './catan-home.component.scss'
+  styleUrl: './catan-home.component.scss',
 })
 export class CatanHomeComponent {
+  readExcel(event: any) {
+    const target: DataTransfer = <DataTransfer>event.target;
+    if (target.files.length !== 1) return;
 
+    const reader: FileReader = new FileReader();
+    reader.onload = (e: any) => {
+      const binaryString: string = e.target.result;
+      const workbook: XLSX.WorkBook = XLSX.read(binaryString, { type: 'binary' });
+
+      const sheetNames: string[] = workbook.SheetNames;
+      console.log('sheetNames:', sheetNames);
+      const sheetName: string = workbook.SheetNames[0];
+
+      const worksheet: XLSX.WorkSheet = workbook.Sheets[sheetName];
+
+      // **セルの値を取得**
+      const cellAddress = 'B2'; // 取得したいセルの位置（例：B2）
+      const cellValue = worksheet[cellAddress]?.v; // 値を取得（?.でnull安全）
+
+      console.log('B2の値:', cellValue);
+    };
+  }
 }
