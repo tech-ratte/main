364fc28c877245f4f203dbdbface053edf7586aa
ratte2d3d <kinoyama001@gmail.com>
2025-12-20 04:17:38 +0900

    ヘルスチェックをカタンのみで行うように修正

--- FrontEnd/src/app/app.component.ts
+++ FrontEnd/src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { coreImports } from '../core/core.imports';
-import { interval, Subscription, takeWhile } from 'rxjs';
-import { HealthCheckService } from '../core/health-check/health-check.service';
 
 @Component({
   selector: 'app-root',
@@ -10,55 +8,4 @@ import { HealthCheckService } from '../core/health-check/health-check.service';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent implements OnInit, OnDestroy {
-  progress = 0;
-  loading = true;
-  progressSub?: Subscription;
-  healthSub?: Subscription;
-
-  constructor(private healthService: HealthCheckService) {}
-
-  ngOnInit() {
-    const totalDuration = 90_000; // 90秒
-    const step = 100; // 100msごと
-    const increment = (100 * step) / totalDuration;
-
-    // プログレスバー進行
-    this.progressSub = interval(step)
-      .pipe(takeWhile(() => this.loading && this.progress < 100))
-      .subscribe(() => {
-        this.progress = Math.min(this.progress + increment, 100);
-      });
-
-    //  最初にヘルスチェック
-    this.healthService.checkHealth().subscribe((ok) => {
-      if (ok) {
-        this.progress = 100;
-        this.loading = false;
-        this.cleanup();
-      }
-    });
-
-    // 5秒ごとのヘルスチェック
-    this.healthSub = interval(5000)
-      .pipe(takeWhile(() => this.loading))
-      .subscribe(() => {
-        this.healthService.checkHealth().subscribe((ok) => {
-          if (ok) {
-            this.progress = 100;   // 一気に 100%
-            this.loading = false;  // ローディング終了
-            this.cleanup();
-          }
-        });
-      });
-  }
-
-  ngOnDestroy() {
-    this.cleanup();
-  }
-
-  private cleanup() {
-    this.progressSub?.unsubscribe();
-    this.healthSub?.unsubscribe();
-  }
-}
+export class AppComponent {}
--- FrontEnd/src/app/catan/catan.component.ts
+++ FrontEnd/src/app/catan/catan.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { coreImports } from '../../core/core.imports';
+import { interval, Subscription, takeWhile } from 'rxjs';
+import { HealthCheckService } from '../../core/health-check/health-check.service';
 
 @Component({
   selector: 'app-catan',
@@ -8,4 +10,55 @@ import { coreImports } from '../../core/core.imports';
   templateUrl: './catan.component.html',
   styleUrl: './catan.component.scss',
 })
-export class CatanComponent {}
+export class CatanComponent implements OnInit, OnDestroy {
+  progress = 0;
+  loading = true;
+  progressSub?: Subscription;
+  healthSub?: Subscription;
+
+  constructor(private healthService: HealthCheckService) {}
+
+  ngOnInit() {
+    const totalDuration = 90_000; // 90秒
+    const step = 100; // 100msごと
+    const increment = (100 * step) / totalDuration;
+
+    // プログレスバー進行
+    this.progressSub = interval(step)
+      .pipe(takeWhile(() => this.loading && this.progress < 100))
+      .subscribe(() => {
+        this.progress = Math.min(this.progress + increment, 100);
+      });
+
+    //  最初にヘルスチェック
+    this.healthService.checkHealth().subscribe((ok) => {
+      if (ok) {
+        this.progress = 100;
+        this.loading = false;
+        this.cleanup();
+      }
+    });
+
+    // 5秒ごとのヘルスチェック
+    this.healthSub = interval(5000)
+      .pipe(takeWhile(() => this.loading))
+      .subscribe(() => {
+        this.healthService.checkHealth().subscribe((ok) => {
+          if (ok) {
+            this.progress = 100;   // 一気に 100%
+            this.loading = false;  // ローディング終了
+            this.cleanup();
+          }
+        });
+      });
+  }
+
+  ngOnDestroy() {
+    this.cleanup();
+  }
+
+  private cleanup() {
+    this.progressSub?.unsubscribe();
+    this.healthSub?.unsubscribe();
+  }
+}
