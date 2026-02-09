bc5178872268051c39aa9cdd2f1d585ea7a17f3a
ratte2d3d <kinoyama001@gmail.com>
2026-02-10 01:03:41 +0900

    削除

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -6,29 +6,9 @@ import { GameComponent } from './catan/game/game.component';
 import { ResultComponent } from './catan/result/result.component';
 import { PlayingGameComponent } from './catan/game/playing-game/playing-game.component';
 import { GameListComponent } from './catan/game-list/game-list.component';
-import { ***REMOVED******REMOVED***Component } from './***REMOVED***/***REMOVED***.component';
-import { TopComponent } from './***REMOVED***/top/top.component';
-import { GraduationProjectComponent } from './***REMOVED***/graduation-project/graduation-project.component';
-import { ProgrammingComponent } from './***REMOVED***/programming/programming.component';
-import { BoardGameManagementComponent } from './***REMOVED***/programming/board-game-management/board-game-management.component';
-import { QuizCppComponent } from './***REMOVED***/programming/quiz-cpp/quiz-cpp.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, title: 'home' },
-  { path: '***REMOVED***',
-    component: ***REMOVED******REMOVED***Component,
-    children: [
-      { path: '', component: TopComponent, title: '***REMOVED*** ***REMOVED***' },
-      { path: 'graduation-project', component: GraduationProjectComponent, title: '***REMOVED*** ***REMOVED*** | 卒業研究' },
-      { path: 'programming',
-        children: [
-          { path: '', component: ProgrammingComponent, title: '***REMOVED*** ***REMOVED*** | 制作物' },
-          { path: 'board-game-management', component: BoardGameManagementComponent, title: '***REMOVED*** ***REMOVED*** | ボードゲームの戦績管理' },
-          { path: 'quiz-cpp', component: QuizCppComponent, title: '***REMOVED*** ***REMOVED*** | クイズアプリケーション' },
-        ],
-      },
-    ],
-  },
   {
     path: 'catan',
     component: CatanComponent,
--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # Webアプリ
-
-- [個人ポートフォリオ](https://frontend-gn26.onrender.com/#/***REMOVED***)
 - [ボードゲームの戦績管理システム](https://frontend-gn26.onrender.com/#/catan)
